65bbf7508aad1d8da211f906756e14116af39aa0
Gift Goma <gomagiftk01@gmail.com>
2024-01-28 14:23:33 +0200

    update

--- mapping-data-to-components/src/components/App.js
+++ mapping-data-to-components/src/components/App.js
@@ -1,26 +1,24 @@
 import React from "react";
 import EmojiCard from "./EmojiCard";
 import { emojiDictionary } from "../EmojiDictionary";
-function createEmojiCard(emojiCard){
-  return <EmojiCard 
-  key = {emojiCard.id}
-  emoji = {emojiCard.emoji}
-  label = {emojiCard.label}
-  meaning = {emojiCard.meaning}
-  
-  />
-}
+
 
 function App() {
   return (
-    <div >
-       <h1> 
-      <span>emojipedia</span>
-    </h1>
-    <div className="grid-container">
-     
-      {emojiDictionary.map(createEmojiCard)}
-    </div>
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <div className="grid-container">
+        {emojiDictionary.map((emojiCard) => (
+          <EmojiCard
+            key={emojiCard.id}
+            emoji={emojiCard.emoji}
+            label={emojiCard.label}
+            meaning={emojiCard.meaning}
+          />
+        ))}
+      </div>
     </div>
   );
 }
